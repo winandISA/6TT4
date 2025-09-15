8dfe74cc2099fbeecc4cc63b3a8975109bd759b6
WinandM <m.winand@ind-philippeville.be>
2025-09-15 12:13:05 +0200

    tictactoe

--- Exercice/Septembre/tictactoe.py
+++ Exercice/Septembre/tictactoe.py
@@ -75,7 +75,7 @@ grid = createGrid()
 a = int("a")
 player = random.choice([True, False])
 #choisi qui joue en premier, si True, c'est le joeur sinon, c'est le cpt
-print("coucou")
+print("coucou3")
 if not player:
     player = place(grid, player)
     print("Le PC commence")
