958646ffad8788c1b6ffe623e9894d76cb3a9f12
Michael <michael.winand@isagosselies.be>
2026-01-23 14:08:00 +0100

    TKinter

---
+++ tkinter/classes/Page/LogIn.py
@@ -0,0 +1,68 @@
+# =========================
+# UI Tkinter
+# =========================
+from tkinter import messagebox, ttk
+
+import tkinter as tk
+
+from exercices.tkinter.classes.Page.BasePage import BasePage
+
+from exercices.tkinter.classes.db.DAO import UserDao
+
+
+class LogIn(BasePage):
+    def __init__(self, parent, controller):
+        self.dao = UserDao()
+
+        parent.title("Login")
+        parent.geometry("500x200")
+        parent.resizable(False, False)
+
+        super().__init__(parent, controller)
+
+        self.frame_connect = tk.LabelFrame(self.root, text="Connexion", padx=10, pady=10)
+        self.frame_connect.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
+
+
+        tk.Label(self.frame_connect, text="Utilisateur :").grid(row=0, column=0, sticky="e", padx=5, pady=5)
+        tk.Label(self.frame_connect, text="Mot de passe :").grid(row=1, column=0, sticky="e", padx=5, pady=5)
+
+        self.combo_user = ttk.Combobox(self.frame_connect, width=22, state="readonly")
+        self.refresh_users()
+
+        self.entry_connect_pwd = tk.Entry(self.frame_connect, width=25, show="*")
+        self.combo_user.grid(row=0, column=1, padx=5, pady=5)
+        self.entry_connect_pwd.grid(row=1, column=1, padx=5, pady=5)
+        self.btn_switch = tk.Button(
+            self.frame_connect,
+            text="Créer un compte",
+            command=self.handle_switch,
+            bg=self.RED,
+            fg="white"
+        )
+        self.btn_switch.grid(row=2, column=0, pady=10)
+        self.btn_connect = tk.Button(self.frame_connect, text="Connect", width=12, command=self.on_connect)
+        self.btn_connect.grid(row=2, column=1, pady=10)
+
+    def on_connect(self):
+        nom = self.combo_user.get().strip()
+        pwd = self.entry_connect_pwd.get().strip()
+        if not nom or not pwd:
+            messagebox.showerror("Erreur", "Nom et mot de passe obligatoires.")
+            return
+        if self.dao.user_exists(nom, pwd):
+            messagebox.showinfo("Connexion", "Accès autorisé ✅")
+        else:
+            messagebox.showerror("Connexion", "Accès refusé ❌")
+
+    def refresh_users(self):
+        users = self.dao.get_all_usernames()
+        self.combo_user["values"] = users
+        if users:
+            self.combo_user.current(0)  # sélectionne le premier
+        else:
+            self.combo_user.set("")  # vide si aucun user
+
+    def handle_switch(self):
+        from exercices.tkinter.classes.Page.CreateUser import CreateUserPage
+        self.controller.show_page(CreateUserPage)
---
+++ tkinter/classes/db/DAO.py
@@ -0,0 +1,54 @@
+import sqlite3
+from abc import ABCMeta, ABC, abstractmethod
+
+from exercices.tkinter.classes.db.DbConnection import DbConnection
+
+class DAO(ABC):
+    def __init__(self):
+        self.db = DbConnection()
+        self.create_table()
+
+    @abstractmethod
+    def create_table(self):
+        pass
+
+# =========================
+# DAO (accès DB)
+# =========================
+class UserDao(DAO):
+    def __init__(self):
+        super().__init__()
+
+    def create_table(self):
+        self.db.execute("""
+        CREATE TABLE IF NOT EXISTS TUser(
+            Id_User INTEGER PRIMARY KEY AUTOINCREMENT,
+            Nom TEXT NOT NULL UNIQUE,
+            Password TEXT NOT NULL
+        );
+        """)
+
+    def add_user(self, nom: str, pwd: str) -> bool:
+        try:
+            self.db.execute(
+                "INSERT INTO TUser(Nom, Password) VALUES (?, ?)",
+                (nom, pwd)
+            )
+            return True
+        except sqlite3.IntegrityError:
+            # Nom déjà existant (UNIQUE)
+            return False
+
+    def user_exists(self, nom: str, pwd: str) -> bool:
+        cur = self.db.execute(
+            "SELECT 1 FROM TUser WHERE Nom = ? AND Password = ?",
+            (nom, pwd)
+        )
+        return cur.fetchone() is not None
+
+    def get_all_usernames(self) -> list[str]:
+        cur = self.db.execute("SELECT Nom FROM TUser ORDER BY Nom")
+        liste = []
+        for row in cur.fetchall():
+            liste.append(row["Nom"])
+        return liste
